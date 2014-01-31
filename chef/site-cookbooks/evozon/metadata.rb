a7edbeb7a565944eef9e464db19756faf1f47066
Botond Dáni <botond.dani@gmail.com>
2014-01-31 12:52:57 +0200

    Updated metadata for cookbooks

--- chef/site-cookbooks/evozon/metadata.rb
+++ chef/site-cookbooks/evozon/metadata.rb
@@ -1,7 +1,7 @@
 name             'evozon'
 maintainer       'Botond Dani'
 maintainer_email 'botond.dani@evozon.com'
-license          'LICENSE'
+license          'Apache 2.0'
 description      'Installs/Configures Evozon Web Standards'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
--- chef/site-cookbooks/magento-ce/metadata.rb
+++ chef/site-cookbooks/magento-ce/metadata.rb
@@ -1,7 +1,7 @@
 name             'magento-ce'
 maintainer       'Botond Dani'
 maintainer_email 'botond.dani@evozon.com'
-license          IO,read(File.join(File.dirname(__FILE__), 'LICENSE'))
+license          'Apache 2.0'
 description      'Installs/Configures Magento Community Edition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
